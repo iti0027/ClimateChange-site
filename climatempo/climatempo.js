3c1569f2fec86c456d0646478652f9609daa3682
Daniel Levi <163361305+odaniellevi@users.noreply.github.com>
2024-12-06 00:03:28 +0000

    importando o link do api

--- climatempo/climatempo.js
+++ climatempo/climatempo.js
@@ -8,4 +8,11 @@ document.querySelector("#search").addEventListener("submit", async (event) => {
       showAlert("Você precisa digitar uma cidade...");
       return;
     }
-  
+
+    const apiKey = "8a60b2de14f7a17c7a11706b2cfcd87c";
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(
+        cityName
+      )}&appid=${apiKey}&units=metric&lang=pt_br`;
+    
+      const results = await fetch(apiUrl);
+      const json = await results.json();
